9befa62d45b5b6f0ce8ff77e7b4891c8aee9ee97
PiotrTekieli <piotr.tekieli@stud.pwsz.legnica.edu.pl>
2022-11-29 15:07:59 +0100

    authentication

---
+++ middleware/authHelpers.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+
+const jwt = require("jsonwebtoken")
+const User = require('../models/user')
+
+verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (!token)
+    return res.status(400).send({ message: "No token provided!" });
+
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err)
+      return res.status(401).send({ message: "Provided token is not valid" })
+
+    let user = User.findById(decoded.id).then((user) => {
+      if (!user)
+        return res.status(401).send({ message: "Provided token is not valid" })
+
+      if (user.logoutTime != decoded.logoutTime)
+        return res.status(401).send({ message: "Provided token is not valid" })
+
+      res.user = user
+      next()
+    })
+  })
+}
+
+checkDuplicateUsers = (req, res, next) => {
+  User.findOne({
+    username: req.body.username
+  }).exec((err, user) => {
+    if (err) {
+        res.status(500).send({ message: err })
+        return
+    }
+
+    if (user) {
+        res.status(400).json({ message: "Username already exists" })
+        return
+    }
+
+    let emailNormalized = req.body.email
+    // emailNormalized = emailNormalized.toLowerCase()
+    User.findOne({
+        email: emailNormalized
+    }).exec((err, user) => {
+        if (err) {
+            res.status(500).send({ message: err })
+            return
+        }
+
+        if (user) {
+            res.status(400).json({ message: "Email already exists" })
+            return
+        }
+
+        next()
+    })
+  })
+}
+
+const authHelpers = {
+  verifyToken,
+  checkDuplicateUsers
+}
+
+module.exports = authHelpers
---
+++ routes/auth.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken')
+
+const { checkDuplicateUsers, verifyToken } = require('../middleware/authHelpers');
+
+
+router.post('/signup', checkDuplicateUsers, async (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8)
+    })
+
+    try {
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+router.post('/login', async (req, res) => {
+    User.findOne({
+        username: req.body.username
+    }).exec((err, user) => {
+        if (err)
+            return res.status(500).send({ message: err })
+
+        if (!user)
+            return res.status(401).json({ message: "Invalid password or username" })
+
+
+        var passwordIsValid = bcrypt.compareSync(
+            req.body.password,
+            user.password
+        )
+
+        if (!passwordIsValid)
+            return res.status(401).send({ message: "Invalid password or username" })
+
+
+        var token = jwt.sign({ id: user.id, logoutTime: user.logoutTime }, process.env.JWT_SECRET, {
+            expiresIn: 86400, // 24 hours
+        });
+
+        res.status(200).json({ message: "Login successful", token: token })
+    })
+})
+
+router.post('/logout', verifyToken, async (req, res) => {
+    res.user.logoutTime = new Date().getTime()
+    try {
+        const loggedOutUser = await res.user.save()
+        res.json(loggedOutUser)
+    } catch (err) {
+        return res.status(500).send({ message: err.message })
+    }
+
+})
+
+router.get("/test", verifyToken, async (req, res) => {
+    res.status(200).json({ message: "Token valid", user: req.user })
+})
+
+module.exports = router
